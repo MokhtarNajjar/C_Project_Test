1b50974f0ace47f4d2a9e64bb7fc230715a8caf6
MokhtarNajjar <45322274+MokhtarNajjar@users.noreply.github.com>
2021-08-16 12:24:01 +0100

    with cin command

--- projectC++1.cpp
+++ projectC++1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main(){
+    // Declare the variables
+    int num;
+    int num2;
+    // Input the integer
+    cout << "Enter the integer: ";
+    cin >> num;
+  
+    // Display the integer
+    cout << "Entered integer is: " << num;
+    
+    cout << "Enter the integer: ";
+    cin >> num2;
     std::cout << "Hello TeamCity!" << std::endl;
     std::cout << "Hello Mokh2 test3!" << std::endl;
     return 0;
