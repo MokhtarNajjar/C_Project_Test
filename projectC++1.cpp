986490a16a1fa1fb1790ab9d1ff511f4bc7c90cc
MokhtarNajjar <45322274+MokhtarNajjar@users.noreply.github.com>
2021-08-16 12:32:17 +0100

    CTestProject+integer reader

--- projectC++1.cpp
+++ projectC++1.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+using namespace std;
 
 int main(){
     // Declare the variables
     int num;
     int num2;
     // Input the integer
-    cout << "Enter the integer: ";
-    cin >> num;
+    std::cout << "Enter the integer: ";
+    std::cin >> num;
   
     // Display the integer
-    cout << "Entered integer is: " << num;
+    std::cout << "Entered integer is: " << num;
     
-    cout << "Enter the integer: ";
-    cin >> num2;
-    std::cout << "Hello TeamCity!" << std::endl;
-    std::cout << "Hello Mokh2 test3!" << std::endl;
+   
+    std::cout << "\n Hello TeamCity!" << std::endl;
+    std::cout << "\n Hello Mokh2 test3!" << std::endl;
+
+     std::cout << "\n Press any key to exit ";
+    std::cin >> num2;
     return 0;
 }
